2c896347113e8a42c5c99373802567ee67a07c4e
Filipe de Mattos Assunção <filipe.assuncao@t2mlab.com>
2022-10-05 22:29:57 -0300

    encapsulamento

--- Program.cs
+++ Program.cs
@@ -1,8 +1,20 @@
 ﻿using exemplopoo.Models;
 
-Pessoa p1 = new Pessoa();
-p1.Nome = "Filipe";
-p1.Idade = 17;
+ContaCorrente c1 = new ContaCorrente(123, 1000);
+c1.ExibirSaldo();
+c1.Sacar(500);
+c1.ExibirSaldo();
 
-p1.Apresentar();
+
+
+
+
+
+
+
+//Pessoa p1 = new Pessoa();
+//p1.Nome = "Filipe";
+//p1.Idade = 17;
+
+//p1.Apresentar();
 
