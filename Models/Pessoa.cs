3f0e49e331fa4218440e304519971a8991e788f7
Filipe de Mattos Assunção <filipe.assuncao@t2mlab.com>
2022-10-05 21:24:05 -0300

    abstração

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -7,6 +7,11 @@ namespace exemplopoo.Models
 {
     public class Pessoa
     {
-        
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public void Apresentar()
+        {
+            Console.WriteLine($"Olá, meu nome é {Nome} e tenho {Idade} anos");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using exemplopoo.Models;
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Filipe";
+p1.Idade = 17;
+
+p1.Apresentar();
+
